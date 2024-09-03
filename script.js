ee5ed8a5a0fbf0bd2cf56678fbf95a9c80ad0be7
JohnReyGimenez <gimenezjr5@gmail.com>
2024-09-03 19:56:55 +0800

    added playround and check winner function

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
-function getComputerChoice() {
+let humanScore = 0
+let computerScore = 0
 const choices = ["rock", "paper", "scissors"]
-const random = Math.floor(Math.random() * choices.length);
-return choices[random]
+
+
+function getComputerChoice() {
+const choice = Math.floor(Math.random() * choices.length);
+return choice;
 }
 
+function playRound(humanSelection, computerSelection) {
+    const result = checkWinner(humanSelection, computerSelection)
+    if(result == "tie") {
+        return "its a tie!"
+    }
+    else if(result == "player") {
+        return `you win! ${humanSelection} beats ${computerSelection}`;
+    }
+    else if(result == "computer") {
+        return `you lose! ${computerSelection} beats ${humanSelection}`
+    }
+}      
+
+
+
 function getHumanChoice() {
-const choices = ["rock", "paper", "scissors"]
 let userInput = prompt("Enter your choice(rock, paper or scissors):")
 if (choices.includes(userInput)) {
     return userInput; }
@@ -15,4 +33,23 @@ if (choices.includes(userInput)) {
     }
 }
 
+function checkWinner() {
+    if(humanSelection == computerSelection) {
+        return "tie";
+    }
+    else if (
+     (humanSelection == "rock" && computerSelection == "scissors") ||
+     (humanSelection == "paper" && computerSelection == "scissors") ||
+     (humanSelection == "scissors" && computerSelection == "rock") 
+    ) {
+    return "human";
+    }
+    else {
+        return "computer";
+    }
+}
+
+const humanSelection = "rock"
+const computerSelection = getComputerChoice
+console.log(playRound(humanSelection, computerSelection))
 
